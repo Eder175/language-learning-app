e0cc6a626d18ab1013cc2c54300dbf4eff7baa63
Eder <souzaeder175@gmail.com>
2025-05-13 22:49:27 +0100

    Ajustando vite.config.ts para usar src como raiz e corrigir erro 404

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'src', // Define src como diretório raiz
   plugins: [react()],
   server: {
     port: 5173,
     open: true, // Abre o navegador automaticamente
   },
   css: {
-    postcss: './postcss.config.js', // Garante que o Tailwind seja processado
+    postcss: '../postcss.config.js', // Caminho relativo ao diretório src
+  },
+  build: {
+    outDir: '../dist', // Saída para builds
   },
 });
